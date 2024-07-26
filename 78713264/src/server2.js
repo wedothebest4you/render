6b618197eee51e1a6b1ae5530c138c1d415cf96e
wedothebest4you <wedothebest4you@gmail.com>
2024-07-26 13:20:50 +0530

    dev change

--- 78713264/src/server2.js
+++ 78713264/src/server2.js
@@ -10,6 +10,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
+
     cookie: { secure: true },
   })
 );
@@ -23,17 +24,24 @@ app.use(
 );
 
 app.get('/cookie', (req, res, next) => {
-  let response;
-  const addinfo = `protocol : ${req.protocol}, host : ${
-    req.hostname
-  }, origin : ${req.get('origin')}`;
-  if (req.session?.somecookie) {
-    response = `Same cookie: A cookie received and the same sent to client - ${addinfo}`;
-  } else {
-    req.session.somecookie = 'cookie text';
-    response = `New cookie: A new cookie created and sent to the client - ${addinfo}`;
-  }
-  res.send(`<h1>${response}</h1>`);
+  // let response;
+  // const addinfo = `protocol : ${req.protocol}, host : ${
+  //   req.hostname
+  // }, origin : ${req.get('origin')}`;
+  // if (req.session?.somecookie) {
+  //   response = `Same cookie: A cookie received and the same sent to client - ${addinfo}`;
+  // } else {
+  //   req.session.somecookie = 'cookie text';
+  //   response = `New cookie: A new cookie created and sent to the client - ${addinfo}`;
+  // }
+  // res.send(`<h1>${response}</h1>`);
+  res.cookie('authToken', token, {
+    maxAge: 24 * 60 * 60 * 1000, // 24 hrs
+    httpOnly: true,
+    secure: true, // Set to true if using HTTPS
+    sameSite: 'None', // Adjust as needed ('Strict', 'Lax', 'None')
+    path: '/',
+  });
 });
 
 app.listen(4000, () => {
